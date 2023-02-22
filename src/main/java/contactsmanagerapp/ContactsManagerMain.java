398edb01512a06bd11d0ef70fa5e4473271a483d
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-02-22 15:47:18 -0600

    added search for contact function and refactored beginning code into start method

--- src/main/java/contactsmanagerapp/ContactsManagerMain.java
+++ src/main/java/contactsmanagerapp/ContactsManagerMain.java
@@ -2,23 +2,60 @@ package contactsmanagerapp;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ContactsManagerMain {
 
     private static final Path contactsFile = Path.of("./src/main/java/contactsmanagerapp/contacts.txt");
 
-    private static ArrayList<Contact> contactList = new ArrayList<>();
+    private static final ArrayList<Contact> contactList = new ArrayList<>();
 
     private static ArrayList<String> contactFileList = new ArrayList<>();
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        startProgram();
+        updateListOfContacts();
+        displayContacts();
+        boolean runProgram = true;
+
+        while (runProgram) {
+            contactMenu();
+            int userChoice = scanner.nextInt();
+            scanner.nextLine();
+//to filter through list
+            switch (userChoice) {
+                case 1:
+                    displayContacts();
+                    break;
+                case 2:
+                    addContact();
+                    updateListOfContacts();
+                    break;
+                case 3:
+                    searchContact();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    runProgram = false;
+                    break;
+                default:
+            }
+        }
+        System.out.println("\n\nGoodbye!");
+    }
+
+    public static void startProgram() {
+        checkForContactsTextFile();
+
+    }
+
+    public static void checkForContactsTextFile() {
         if (Files.notExists(contactsFile)) {
             try {
                 Files.createFile(contactsFile);
@@ -26,32 +63,9 @@ public class ContactsManagerMain {
                 System.out.println("Could not create contacts.txt.");
             }
         }
-
-        updateListOfContacts();
-        displayContacts();
-        contactMenu();
-
-        int userChoice = scanner.nextInt();
-        scanner.nextLine();
-//to filter through list
-        switch (userChoice) {
-            case 1:
-                displayContacts();
-                break;
-            case 2:
-                addContact();
-                updateListOfContacts();
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default:
-        }
     }
-//array list that adds new names and updates contacts
+
+    //array list that adds new names and updates contacts
     public static void updateListOfContacts() {
         contactFileList.clear();
         contactList.clear();
@@ -63,7 +77,7 @@ public class ContactsManagerMain {
                 System.out.println("List of Contacts:");
                 for (String element : contactFileList) {
                     if (element.equals("")) {
-//remove line
+                        //remove line
                         continue;
                     }
                     contactList.add(Contact.fromFileString(element));
@@ -72,8 +86,6 @@ public class ContactsManagerMain {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Displaying contact objects: " + contactList);
-
     }
 
     public static void displayContacts() {
@@ -81,6 +93,7 @@ public class ContactsManagerMain {
             System.out.println(contact);
         }
     }
+
     //menu
     public static void contactMenu() {
         System.out.println("""
@@ -94,8 +107,9 @@ public class ContactsManagerMain {
 
 
     }
+
     //adds new contacts and phone numbers
-    public static void addContact(){
+    public static void addContact() {
         System.out.println("Name of contact");
         String newContactName = scanner.nextLine();
 
@@ -105,13 +119,53 @@ public class ContactsManagerMain {
         try {
             Files.write(
                     contactsFile,
-                    Arrays.asList("\n" + newContactName + "|" + newContactNumber),
+                    List.of("\n" + newContactName + "|" + newContactNumber),
                     StandardOpenOption.APPEND
             );
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void searchContact() {
+        System.out.println("Would you like to search by name or number? (\"name\" or \"number\" ");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equalsIgnoreCase("name")) {
+            System.out.println("Please enter the name of the contact.");
+            userInput = scanner.nextLine();
+            boolean isFound = false;
+            for (Contact contact : contactList) {
+                if (contact.getName().equalsIgnoreCase(userInput)) {
+                    isFound = true;
+                    System.out.println(userInput + "'s phone number is " + contact.getPhoneNumber());
+                    break;
+                }
+            }
+            if (!isFound) {
+                System.out.println("Could not find contact.");
+            }
+
+        } else if (userInput.equalsIgnoreCase("number")) {
+            System.out.println("Please enter the number of the contact.");
+            userInput = scanner.nextLine();
+            int userNumInput = Integer.parseInt(userInput);
+            boolean isFound = false;
+            for (Contact contact : contactList) {
+                if (contact.getPhoneNumber() == userNumInput) {
+                    isFound = true;
+                    System.out.println(userNumInput + " belongs to " + contact.getName());
+                    break;
+                }
+            }
+            if (!isFound) {
+                System.out.println("Could not find contact.");
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+
 }
 
 //e.printStackTrace(); tracks errors
